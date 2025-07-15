8a41f48948bd3dc2ddf5632743bd97837b6fbbbe
Axel Castaño <axelcastano@proton.me>
2025-07-15 19:23:41 -0300

    Updated server links

--- src/config/server-invites.ts
+++ src/config/server-invites.ts
@@ -1,14 +1,14 @@
 import type { ServerKey } from '~/types/config.js';
 
 export const serverInvites: Record<ServerKey, string> = {
-  rivals: 'https://discord.gg/bloxyrivals',
+  rivals: 'https://discord.gg/DYx2uzUK5c',
   'blox-fruits': 'https://discord.gg/bloxyfruit',
-  'bloxy-market': 'https://discord.gg/fyTQNnbW7B',
-  'grow-a-garden': 'https://discord.gg/fkmpNAb3',
-  'pets-go': 'https://discord.gg/bloxypets',
+  'bloxy-market': 'https://discord.gg/JuYbRcSqsB',
+  'grow-a-garden': 'https://discord.gg/bloxygag',
+  'pets-go': 'https://discord.gg/th2nRcJCef',
   'anime-vanguards': 'https://discord.gg/5gwAZZpXSw',
   'king-legacy': 'https://discord.gg/8sjBPzzxwA',
   'blue-lock': 'https://discord.gg/u53cFFR95H',
-  'steal-brainrot': 'https://discord.gg/tZmHFhud',
+  'steal-brainrot': 'https://discord.gg/xhEXNBkS7w',
   'murder-mystery-2': 'https://discord.gg/WG826HDXbe',
 } as const;
