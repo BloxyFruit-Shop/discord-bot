82985781e68c3274533fae499a5b7023b8d887e7
Axel Castaño <axelcastano@proton.me>
2025-10-26 00:07:26 -0300

    Fixed broken invite url

--- src/config/server-invites.ts
+++ src/config/server-invites.ts
@@ -10,5 +10,5 @@ export const serverInvites: Record<ServerKey, string> = {
   'king-legacy': 'https://discord.gg/8sjBPzzxwA',
   'blue-lock': 'https://discord.gg/u53cFFR95H',
   'steal-brainrot': 'https://discord.gg/xhEXNBkS7w',
-  'plants-vs-brainrot': 'https://discord.gg/u4Pc84kT',
+  'plants-vs-brainrot': 'https://discord.gg/HVPt5ceHan',
 } as const;
